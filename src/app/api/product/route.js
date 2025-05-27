1f40c107f74e3689548a3756e6b446b735a8fd50
suthakaranburaj <suthakaranburaj@gmail.com>
2025-05-27 18:37:53 +0530

    search , pagination , limit added for get_all_products api . may 27

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -3,24 +3,43 @@ import { send_response } from "@/utils/apiResponse";
 import dbConnect from "@/lib/db";
 import { asyncHandler } from "@/utils/asyncHandler";
 import { StatusCodes } from "@/helper/api/statusCode";
-import { ProductImages } from "@/models/Product_related_images";
 
 export const GET = asyncHandler(async (req) => {
   await dbConnect();
   const { searchParams } = new URL(req.url);
+
   const id = searchParams.get("id");
-  // console.log(searchParams)
-  // console.log(id);
+  const page = Math.max(parseInt(searchParams.get("page")) || 1, 1);
+  const limit = Math.max(parseInt(searchParams.get("limit")) || 10, 1);
+  const searchTerm = searchParams.get("searchTerm");
+
+  const skip = (page - 1) * limit;
+
   const matchStage = {
     show_on_website: true,
-    ...(id && { category_id: id }), // only include category_id match if id is present
+    ...(id && { category_id: id }),
   };
-  // console.log("fweofhoweifh", matchStage.category_id);
-  const productsWithImages = await Product.find(matchStage);
+
+  if (searchTerm) {
+    const searchRegex = new RegExp(searchTerm, "i");
+    matchStage.$or = [{ name: searchRegex }];
+  }
+
+  const total = await Product.countDocuments(matchStage);
+  const totalPages = Math.ceil(total / limit);
+  const products = await Product.find(matchStage).skip(skip).limit(limit);
 
   return send_response(
     true,
-    productsWithImages,
+    {
+      products,
+      pagination: {
+        total,
+        totalPages,
+        currentPage: page,
+        limit,
+      },
+    },
     "Products fetched successfully.",
     StatusCodes.OK
   );
